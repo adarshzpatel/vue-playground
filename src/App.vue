6ce67064a6991a94a3f1332588c6c34b4a1f4acd
adarshzpatel@gmail.com <adarshzpatel@gmail.com>
2021-12-05 13:58:56 +0530

    Button Theming

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,109 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <h1>Hello</h1>
+ <Button color='primary'>Primary</Button>
+ <Button color='secondary'>Secondary</Button>
+ <Button color='info'>Info</Button>
+ <Button @click="toggleDarkMode" color='secondary'>Toogle Dark Mode</Button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Button from "./components/Button.vue"
 export default {
   name: 'App',
+  methods: {
+    toggleDarkMode(){
+      const app = document.getElementById('app');
+      if(app.classList.contains('dark')){
+        app.classList.remove('dark');
+      } else {
+        app.classList.add('dark');
+      }
+    }
+  },
   components: {
-    HelloWorld
-  }
+    Button
+}
 }
 </script>
 
 <style lang="scss">
+
+*{
+  margin: 0;
+  padding: 0;
+}
+
+:root {
+  --background: #fffffe !important;
+  --primary:#4FC4CF !important;
+  --primary-light:#77D1DA !important;
+  --primary-dark:#32ADB8 !important;
+  --secondary: #994ff3 !important;
+  --secondary-light:#B47EF6 !important;
+  --secondary-dark:#7D1FEF !important;
+  --text-primary: #181818 !important;
+  --info: #00b0ff !important;
+  --info-light: #33bfff !important;
+  --info-dark: #007bb2;
+
+}
+
+.dark{
+  --background: #16161a !important;
+  --primary: #2cb67d !important;
+  --primary-light:#42D196 !important;
+  --primary-dark:#228C60 !important;
+  --secondary: #7f5af0 !important;
+  --secondary-light:#A489F5 !important;
+  --secondary-dark:#5D2DEB !important;
+  --text-primary: #fffffe !important;
+   --info: #00b0ff !important;
+  --info-light: #33bfff !important;
+  --info-dark: #007bb2;
+}
+
+.primary{
+  background: var(--primary);
+  &:hover{
+    background: var(--primary-light);
+  }
+  &:active{
+    background: var(--primary-dark);
+  }
+}
+
+.secondary{
+  background: var(--secondary);
+  &:hover{
+    background: var(--secondary-light);
+  }
+  &:active{
+    background: var(--secondary-dark);
+  } 
+}
+
+.info{
+  background: var(--info);
+  &:hover{
+    background: var(--info-light);
+  }
+  &:active{
+    background: var(--info-dark);
+  } 
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  gap: 1rem;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+  color: var(--text-primary);
+  background: var(--background);
 }
 </style>
---
+++ src/components/Button.vue
@@ -0,0 +1,31 @@
+<template>
+  <button class="lv-button" v-bind="$attrs" :class="color"  >
+    <slot></slot>  
+  </button>  
+</template>
+
+<script>
+export default{
+  name:'Button',
+  props:{
+    color:{
+      type: String,
+      default: 'primary'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+.lv-button {
+    box-shadow: none !important;
+    padding: 12px 20px !important;
+    white-space: nowrap;
+    border: none;
+    border-radius: 4px;
+    font-size: medium;
+    font-weight: 500;
+    color: #fffffe;
+  }
+</style>
