e2fb6a4d9ee3e52bd9b313fa7ed74f25d756cff7
adarshzpatel@gmail.com <adarshzpatel@gmail.com>
2021-12-13 18:46:00 +0530

    theme generator

--- src/App.vue
+++ src/App.vue
@@ -1,79 +1,90 @@
 <template>
- <h1>Hello</h1>
- <Button color='primary'>Primary</Button>
- <Button color='secondary'>Secondary</Button>
- <Button color='info'>Info</Button>
- <Button @click="toggleDarkMode" color='secondary'>Toogle Dark Mode</Button>
+  <div class="container">
+    <button class="primary-light">Primary Light</button>
+    <button>Primary</button>
+    <button class="primary-dark">Primary Dark</button>
+  </div>
+<p>Hue : {{hue}} </p>
+  <input v-model="hue" type="range" max="360"  name="hue" id="hue" />
 </template>
 
 <script>
-import Button from "./components/Button.vue"
 export default {
-  name: 'App',
-  methods: {
-    toggleDarkMode(){
-      const app = document.getElementById('app');
-      if(app.classList.contains('dark')){
-        app.classList.remove('dark');
-      } else {
-        app.classList.add('dark');
-      }
-    }
+  name: "App",
+  data() {
+    return {
+      hue: 0,
+    };
   },
-  components: {
-    Button
-}
-}
+  watch:{
+    // setTheme(){
+      hue(){
+        document.documentElement.style.setProperty('--primary-h',this.hue);
+        }
+    // }
+  }
+};
 </script>
 
 <style lang="scss">
-
-*{
-  margin: 0;
-  padding: 0;
-}
-
 :root {
-  --background: #fffffe !important;
-  --primary:#4FC4CF !important;
-  --primary-light:#77D1DA !important;
-  --primary-dark:#32ADB8 !important;
-  --secondary: #994ff3 !important;
-  --secondary-light:#B47EF6 !important;
-  --secondary-dark:#7D1FEF !important;
-  --text-primary: #181818 !important;
-  --info: #00b0ff !important;
-  --info-light: #33bfff !important;
-  --info-dark: #007bb2;
+  /* Constants */
+  --lightnessTransform: 10%;
+  /* Base Values */
+  --primary-h: 180;
+  --primary-s: 100%;
+  --primary-l: 40%;
 
+  /* Variant Values */
+  --primary-light-l: calc(var(--primary-l) + var(--lightnessTransform));
+  --primary-dark-l: calc(var(--primary-l) - var(--lightnessTransform));
+  /* Composed Primary Color*/
+  --primary: hsl(var(--primary-h), var(--primary-s), var(--primary-l));
+  /* Primary Light Variant */
+  --primary-light: hsl(
+    var(--primary-h),
+    var(--primary-s),
+    var(--primary-light-l)
+  );
+  --primary-dark: hsl(
+    var(--primary-h),
+    var(--primary-s),
+    var(--primary-dark-l)
+  );
+  --text-color: white;
+  --border-radius: 4px;
+}
+button {
+  background: var(--primary);
+  padding: 12px 16px;
+  border-radius: var(--border-radius);
+  border: none;
+  color: var(--text-color);
+  font-size: larger;
+  white-space: nowrap;
+  &:hover {
+    transition: all 0.3s ease-out;
+    background: var(--primary-light);
+  }
+  &:active {
+    transition: all 0.3s ease-out;
+    transform: translateY(2px);
+    background: var(--primary-dark);
+  }
 }
 
-.dark{
-  --background: #16161a !important;
-  --primary: #2cb67d !important;
-  --primary-light:#42D196 !important;
-  --primary-dark:#228C60 !important;
-  --secondary: #7f5af0 !important;
-  --secondary-light:#A489F5 !important;
-  --secondary-dark:#5D2DEB !important;
-  --text-primary: #fffffe !important;
-   --info: #00b0ff !important;
-  --info-light: #33bfff !important;
-  --info-dark: #007bb2;
+.primary-light {
+  background: var(--primary-light);
 }
 
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+.primary-dark {
+  background: var(--primary-dark);
+}
+
+.container {
   display: flex;
-  gap: 1rem;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 100vh;
-  color: var(--text-primary);
-  background: var(--background);
+  flex-wrap: wrap;
+  gap: 10px;
+  margin:1rem;
 }
 </style>
